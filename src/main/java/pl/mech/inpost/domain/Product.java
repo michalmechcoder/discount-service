d6a8cb4bb0485ebe12b2af756b623186a3db7f29
Michał Mech <michal.mech@gmail.com>
2024-03-06 22:10:22 +0100

    Missing constructor

--- src/main/java/pl/mech/inpost/domain/Product.java
+++ src/main/java/pl/mech/inpost/domain/Product.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
 @Entity
+@NoArgsConstructor
 @AllArgsConstructor
 public class Product {
 
